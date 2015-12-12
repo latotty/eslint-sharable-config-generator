51798e600c9184ebb8aa3a6afe1c9b0070213a91
LaTotty <latotty91@gmail.com>
2015-12-12 04:29:21 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# eslint-sharable-config-generator
+# Eslint sharable config generator
+
+Install with `npm install eslint-sharable-config-generator`
+
+## Usage
+
+Run manually
+
+`./node_modules/.bin/eslint-sharable-config-generator src .` (if you stored your config files at `./config`, and you want your compiled js files in the cwd).
+
+Or add it to npm:
+
+```
+"scripts": {
+  "build": "eslint-sharable-config-generator src ."
+},
+```
+You do not need to include `./node_modules/.bin/` in npm scripts.
+
+## --help
+
+Usage: `eslint-sharable-config-generator <from-path> <to-path>`
+
+  Generates compiled js eslint config files.
+
+  Config files can be:
+    javascript (.js)
+    json       (.json)
+    yaml       (.yml and .yaml)
+
+  Files with name starting with underscore (_)
+    or files in subdirectories will be ignored.
+
+Options:
+
+  `-h`, `--help`    Display this usage info
---
+++ index.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fs = require('fs');
+const glob = require('glob');
+const path = require('path');
+
+const args = process.argv.slice(2);
+
+function printHelp(isHelp) {
+  const log = isHelp ? 'log' : 'error';
+  console[log]('Usage: eslint-sharable-config-generator <from-path> <to-path>');
+  console[log]('');
+  console[log]('  Generates compiled js eslint config files.');
+  console[log]('');
+  console[log]('  Config files can be:');
+  console[log]('    javascript (.js)');
+  console[log]('    json       (.json)');
+  console[log]('    yaml       (.yml and .yaml)');
+  console[log]('');
+  console[log]('  Files with name starting with underscore (_)');
+  console[log]('    or files in subdirectories will be ignored.');
+  console[log]('');
+  console[log]('Options:');
+  console[log]('');
+  console[log]('  -h, --help    Display this usage info');
+  process.exit(isHelp ? 0 : 1);
+}
+
+if (~args.indexOf("--help") || ~args.indexOf("-h")) {
+  printHelp(true);
+}
+
+if (args.length != 2) {
+  printHelp(false);
+}
+
+const fromDir = args[0];
+const toDir = args[1];
+
+try {
+  fs.accessSync(fromDir);
+  fs.accessSync(toDir);
+} catch (e) {
+  console.error(`Invalid path.\n`);
+  printHelp(false);
+}
+
+const eslintConfigFile = require('eslint/lib/config/config-file.js');
+const eslintConfigOps = require('eslint/lib/config/config-ops.js');
+
+function compileConfig(from, to) {
+  const eslintConfigOpsApplyEnvironmentsBak = eslintConfigOps.applyEnvironments;
+  eslintConfigOps.applyEnvironments = (config) => config; // HAX
+
+  const config = eslintConfigFile.load(path.resolve(from));
+  delete config.extends;
+  eslintConfigFile.write(config, path.resolve(to));
+
+  eslintConfigOps.applyEnvironments = eslintConfigOpsApplyEnvironmentsBak;
+}
+
+glob.sync('./*.@(js|json|yml|yaml)', { ignore: './_*', cwd: fromDir }).forEach((filePath) => {
+  const fromAbsolute = path.resolve(filePath);
+  const fromExt = path.extname(filePath);
+  const toFileName = path.basename(filePath).replace(fromExt, '.js');
+  const toAbsolute = path.resolve(toFileName);
+  compileConfig(fromAbsolute, toAbsolute);
+});
